c2529f860214ce7a6236dcf5b7866edd0b6ccebe
tony2550 <tony7320@gmail.com>
2022-03-14 15:15:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import Hello from './Hello';
 import './App.css';
 
-function App() {
+const App = () => {
+  const draggingItem = useRef();
+  const [list, setList] = useState([
+    'Lotte Giant',
+    'Samsung Lions',
+    'SSG Randers',
+    'Hanhwa Eagles',
+    'Doosan Bears',
+    'NC Dinos',
+    'KT Wiz',
+    'KIA Tigers',
+    'Kiwoom Heroes',
+    'LG Twins',
+  ]);
+  const textValue = 'mookie react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 30,
+    padding: '10px',
+  };
+
+  const handleDragStart = (e, position) => {
+    draggingItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={style}>{textValue}</div>
+      <Hello />
+      {list &&
+        list.map((item, index) => (
+          <h1 key={index} draggable>
+            {item}
+          </h1>
+        ))}
+    </>
   );
-}
+};
 
 export default App;
